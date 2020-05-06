e28e5b4dc8f15b485a1fab79d353e83ae4119d3c
System Administrator <adrian.hoffmann@slalom.com>
2020-05-06 12:28:53 -0700

    added slasher service check

--- roles/eth-deploy/templates/status-check.sh
+++ roles/eth-deploy/templates/status-check.sh
@@ -4,8 +4,10 @@ topic_arn="arn:aws:sns:us-west-1:810013023562:services-check"
 sqs_url=""
 #beacon_status=$(ps -aux | grep -w "\/beacon-chain" | echo $?)
 #validator_status=$(ps -aux | grep -w "\/validator" | echo $?)
+#slasher_status=$(ps -aux | grep -w "prysm.sh slasher" | echo $?)
 beacon_service=$(systemctl status eth-beacon | grep -ohP "(?<=Active:\s)([a-z]*)(\b)")
 validator_service=$(systemctl status eth-validator | grep -ohP "(?<=Active:\s)([a-z]*)(\b)")
+#slasher_service=$(systemctl status eth-slasher| grep -ohP "(?<=Active:\s)([a-z]*)(\b)")
 instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 timestamp=$(date)
 disk_state=$(df -k /boot | grep -ohP "[0-9]?[0-9]?[0-9]%")
